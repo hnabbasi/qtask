1f50095ab07660f0d7aa051de7c190ca3b0cad76
Hussain Abbasi <hnabbasi@outlook.com>
2020-03-25 23:21:02 -0500

    Demo UI

---
+++ src/QTask/QTask.iOS/Renderer/NavigationPageRenderer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using QTask.iOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly:ExportRenderer(typeof(NavigationPage), typeof(NavigationPageRenderer))]
+namespace QTask.iOS.Renderers
+{
+    public class NavigationPageRenderer : NavigationRenderer
+    {
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+        }
+    }
+}
---
+++ src/QTask/QTask/ViewModels/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using MvvmHelpers;
+using Prism.Commands;
+using QTask.Models;
+
+namespace QTask.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public MainViewModel()
+        {
+            Title = "My Tasks";
+            CompleteCommand = new DelegateCommand(OnCompleteTapped);
+            GetTasks();
+        }
+
+        public List<TaskItem> Tasks { get; set; }
+        public DelegateCommand CompleteCommand { get; }
+
+        private void OnCompleteTapped()
+        {
+            Debug.WriteLine("Dairy completed");
+        }
+
+        private void GetTasks()
+        {
+            Tasks = new List<TaskItem>
+            {
+                new TaskItem { Id = Guid.NewGuid(), Title = "Keep social distance" },
+                new TaskItem { Id = Guid.NewGuid(), Title = "Drink water" },
+                new TaskItem { Id = Guid.NewGuid(), Title = "Wash hands regularly" },
+                new TaskItem { Id = Guid.NewGuid(), Title = "Meditate" },
+                new TaskItem { Id = Guid.NewGuid(), Title = "Call family and checkup" },
+                new TaskItem { Id = Guid.NewGuid(), Title = "Read books" }
+            };
+        }
+    }
+}
