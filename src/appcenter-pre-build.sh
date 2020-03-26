bf548520c33b2344ffd10b49bac094e31fdd607e
Hussain Abbasi <hnabbasi@outlook.com>
2020-03-26 16:01:31 -0500

    Full version name fixed

--- src/appcenter-pre-build.sh
+++ src/appcenter-pre-build.sh
@@ -114,8 +114,8 @@ fi
 
 echo "Version name from variable: $VERSION_NAME"
 echo "Build ID from env. variable: $APPCENTER_BUILD_ID"
-echo "Should be: $VERSION_NAME.$APPCENTER_BUILD_ID"
-FULL_VERSION_NAME = "$VERSION_NAME.$APPCENTER_BUILD_ID"
+
+FULL_VERSION_NAME="$VERSION_NAME.$APPCENTER_BUILD_ID"
 
 if [ -e "$FULL_VERSION_NAME" ]
 then
